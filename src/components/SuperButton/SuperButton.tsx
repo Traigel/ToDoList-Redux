9e1b0add7fda3b782983cc5b196ed5a02b74a7af
Vova Traihel <vovatraigel@gmail.com>
2022-06-28 12:44:45 +0300

    Button stories

--- src/components/SuperButton/SuperButton.tsx
+++ src/components/SuperButton/SuperButton.tsx
@@ -2,15 +2,41 @@ import React from 'react';
 import styles from './SuperButton.module.css'
 
 type ButtonType = {
-    buttonName: string,
+    buttonName: string
     callBack: () => void
+    /**
+     * Set button style from outside
+     */
+    className?: string
+    /**
+     * Button text color
+     */
+    nameColor?: string
+    /**
+     * Background color
+     */
+    backgroundColor?: string
+    /**
+     * Button text size
+     */
+    size?: 'small' | 'medium' | 'large'
 }
 
 export const SuperButton = (props: ButtonType) => {
 
     const onclickButtonHandler = () => props.callBack()
 
+    const stylesButton = {
+        color: props.nameColor ? props.nameColor : 'black',
+        backgroundColor: props.backgroundColor ? props.backgroundColor : '',
+        fontSize: props.size ? props.size : ''
+    }
+
     return (
-            <button onClick={onclickButtonHandler}>{props.buttonName}</button>
+        <button style={stylesButton}
+                className={props.className}
+                onClick={onclickButtonHandler}>
+            {props.buttonName}
+        </button>
     )
 }
